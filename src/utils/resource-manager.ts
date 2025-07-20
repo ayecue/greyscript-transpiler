3cd66bacbaccda68f0c8f90fa1923331382d8bc1
ayecue <soerenwehmeier@googlemail.com>
2025-07-20 08:48:01 +0200

    1.8.1 improve error on missing dependency

--- src/utils/resource-manager.ts
+++ src/utils/resource-manager.ts
@@ -4,6 +4,7 @@ import {
   ResourceManagerLike
 } from 'greybel-transpiler';
 import { ASTChunkGreyScript } from 'greyscript-core';
+import { ASTBase } from 'miniscript-core';
 
 export class ResourceManager
   extends GreybelResourceManager
@@ -12,29 +13,44 @@ export class ResourceManager
   protected async enrichResource(resource: Resource): Promise<void> {
     const { imports, includes, nativeImports, injects } =
       resource.chunk as ASTChunkGreyScript;
-    const resourcePaths = await Promise.all([
+    const resourcePaths: { ref: ASTBase, resolved: string }[] = await Promise.all([
       ...imports.map(async (item) => {
-        return this.createMapping(resource.target, item.path);
+        return {
+          ref: item,
+          resolved: await this.createMapping(resource.target, item.path)
+        };
       }),
       ...includes.map(async (item) => {
-        return this.createMapping(resource.target, item.path);
+        return {
+          ref: item,
+          resolved: await this.createMapping(resource.target, item.path)
+        };
       }),
       ...nativeImports
         .filter((item) => {
           return item.eval && item.emit;
         })
         .map(async (item) => {
-          return this.createMapping(resource.target, item.directory);
+          return {
+            ref: item,
+            resolved: await this.createMapping(resource.target, item.directory)
+          };
         })
     ]);
 
     await Promise.all([
       ...resourcePaths.map(async (resourcePath) => {
-        await this.loadResource(resourcePath);
+        await this.loadResource(resourcePath.resolved, {
+          target: resource.target,
+          ref: resourcePath.ref
+        });
       }),
       ...injects.map(async (item) => {
         const depTarget = await this.createMapping(resource.target, item.path);
-        await this.createInjection(depTarget);
+        await this.createInjection(depTarget, {
+          target: resource.target,
+          ref: item
+        });
       })
     ]);
   }
